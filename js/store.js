fc8f6ae06f4b392393216f77cd5d2a652702a5d8
Ranmocy Sheng <ranmocy@gmail.com>
2015-10-21 16:42:56 -0700

    add dropbox component

--- js/store.js
+++ js/store.js
@@ -1,27 +1,47 @@
 (function () {
   "use strict";
 
+  var CONFIG_FIELD = 'config';
+  var ACCOUNTS_FIELD = 'accounts';
+
   class Store {
     constructor() {
       this.cache = {};
     }
 
+    getJSON(name, defaultValue) {
+      if (this.cache[name]) {
+        return this.cache[name];
+      }
+      var data = JSON.parse(localStorage.getItem(name));
+      if (!data) {
+        data = defaultValue;
+      }
+      this.cache[name] = data;
+      return data;
+    }
+
+    saveJSON(name, value) {
+      this.cache[name] = value;
+      localStorage.setItem(name, JSON.stringify(value));
+      return value;
+    }
+
     getConfig() {
-      if (this.cache.config) {
-        return this.cache.config;
-      }
-      var config = JSON.parse(localStorage.getItem('config'));
-      if (!config) {
-        config = {};
-      }
-      this.cache.config = config;
-      return config;
+      return this.getJSON(CONFIG_FIELD, {});
     }
 
     saveConfig(config) {
-      this.cache.config = config;
-      localStorage.setItem('config', JSON.stringify(config));
-      return config;
+      return this.saveJSON(CONFIG_FIELD, config);
+    }
+
+    getAccounts() {
+      var accounts = this.getJSON(ACCOUNTS_FIELD, []);
+      return accounts;
+    }
+
+    saveAccounts(accounts) {
+      return this.saveJSON(ACCOUNTS_FIELD, accounts);
     }
   }
 

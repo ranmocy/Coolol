bcca66778827aa377faa2b75fad315a971f1fb9d
Ranmocy Sheng <ranmocy@gmail.com>
2015-10-23 22:40:33 -0700

    update release

--- release.rb
+++ release.rb
@@ -3,16 +3,18 @@
 require 'fileutils'
 
 BUILD_PATH = '.build'
+PROJECT_URL = `git config --get remote.github.url`.strip
 TARGET_BRANCH='gh-pages'
 
-$VERBOSE = true
+puts PROJECT_URL
 
 SILENT = ($VERBOSE) ? "" : ">/dev/null 2>/dev/null"
 def cmd(s, name=nil)
+  puts "run: #{s}"
   system("#{s} #{SILENT}") ? puts("Success: #{name}") : abort("Failed: #{name}")
 end
 
-FileUtils.rm_r(BUILD_PATH, secure: true) if File.exists? BUILD_PATH
+FileUtils.rm_r(BUILD_PATH, secure: true) if File.exist? BUILD_PATH
 FileUtils.mkdir_p(BUILD_PATH)
 
 Dir.glob("*") { |file| cmd "cp -r #{file} #{BUILD_PATH}", "CP:#{file}" }
@@ -20,5 +22,5 @@ Dir.chdir BUILD_PATH do
   cmd "git init"
   cmd "git add --all .", 'add all'
   cmd "git commit -m 'init'", 'commit'
-  cmd "git push https://github.com/ranmocy/Coolol master:#{TARGET_BRANCH} --force"
+  cmd "git push #{PROJECT_URL} master:#{TARGET_BRANCH} -f", 'push'
 end

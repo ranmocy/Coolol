a6e47051eb4b53b34cc6c25dffe2ef8c4d3dff63
Ranmocy Sheng <ranmocy@gmail.com>
2016-10-29 19:02:12 -0700

    add spec of cache indexeddb access

---
+++ spec/cache_spec.js
@@ -0,0 +1,63 @@
+let $suite = window.new_test_suite(document.currentScript.src);
+let $test_put = window.new_test($suite, 'Cache Table Access');
+
+
+function getRequestPromise(request) {
+  return new Promise((resolve, reject) => {
+    console.log('set request', request);
+    window.request = request;
+    request.onsuccess = (event) => {
+      console.log('promise success');
+      resolve(event.target.result);
+    };
+    request.onerror = (event) => {
+      console.log('promise error');
+      reject(event.target.errorCode);
+    };
+  });
+}
+
+document.account = {screen_name: 'ranmocy'};
+
+let cache = document.cache;
+
+let tweets = [
+  {id_str: '1234', text: 'I love it.'},
+  {id_str: '3123', text: 'I love it too.'},
+  {id_str: '1234', text: 'I love it three.'},
+];
+
+console.debug('start!');
+cache
+  .putTweets(tweets)
+  .then(() => {
+    return cache.getTweets(['3123', '1234', '4444']);
+  })
+  .then((tweets) => {
+    console.debug('read', tweets);
+    if ((tweets[0].text !== 'I love it too.') ||
+        (tweets[1].text !== 'I love it three.') ||
+        (tweets[2] !== undefined)) {
+      throw 'Expected tweets';
+    }
+  })
+  .then(() => {
+    console.debug('Removing!!!');
+    return getRequestPromise(window.indexedDB.deleteDatabase('Coolol'));
+  })
+  .then(() => {
+    console.log('Check Databases');
+    return getRequestPromise(window.indexedDB.webkitGetDatabaseNames());
+  })
+  .then((names) => {
+    console.log('Databases', names);
+    if (names.length > 0) {
+      throw 'Database is not deleted';
+    }
+  })
+  .then(() => {
+    $test_put.className = 'success';
+  })
+  .catch(() => {
+    $test_put.className = 'failed';
+  });
